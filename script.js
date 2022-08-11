0087b5eed32c5a28ddf550c0b547a0f29b51d000
keetabikeedaq <99006216+keetabikeedaq@users.noreply.github.com>
2022-08-11 14:19:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function greet() {
+
+    document.getElementById("myDIV").classList.toggle("hide");
+    document.getElementById("messgH").classList.toggle("hide");
+   document.getElementById("messg").classList.toggle("dis");
+   document.getElementById("bhid").classList.toggle("hide");
+    
+    }
+  
